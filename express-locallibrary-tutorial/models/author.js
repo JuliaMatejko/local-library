91d811f056d68cc55e4b31e94a999a2e90de5cba
Julia Matejko <matejkojulia@gmail.com>
2025-02-18 01:22:58 +0100

    Add book instance detail page and controller method. Add author model virtual property 'lifespan'

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -36,5 +36,11 @@ AuthorSchema.virtual("date_of_death_formatted").get(function () {
   return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
+AuthorSchema.virtual("lifespan").get(function () {
+  const birthDate = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  const deathDate = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+  return birthDate + ' - ' + deathDate;
+});
+
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
